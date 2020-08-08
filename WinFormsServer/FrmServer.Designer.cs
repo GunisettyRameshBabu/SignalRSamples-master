3895402493e517e75f37d635001f812b7c1c014e
Acharyya <n073697@global.ual.com>
2020-08-07 23:27:17 -0500

    Added Socket Connection Logic

--- WinFormsServer/FrmServer.Designer.cs
+++ WinFormsServer/FrmServer.Designer.cs
@@ -36,14 +36,15 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.grpBroadcast = new System.Windows.Forms.GroupBox();
-            this.rdToAll = new System.Windows.Forms.RadioButton();
-            this.rdToGroup = new System.Windows.Forms.RadioButton();
-            this.rdToClient = new System.Windows.Forms.RadioButton();
-            this.cmbGroups = new System.Windows.Forms.ComboBox();
-            this.cmbClients = new System.Windows.Forms.ComboBox();
+            this.btnSend = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.txtMessage = new System.Windows.Forms.TextBox();
-            this.btnSend = new System.Windows.Forms.Button();
+            this.cmbClients = new System.Windows.Forms.ComboBox();
+            this.cmbGroups = new System.Windows.Forms.ComboBox();
+            this.rdToClient = new System.Windows.Forms.RadioButton();
+            this.rdToGroup = new System.Windows.Forms.RadioButton();
+            this.rdToAll = new System.Windows.Forms.RadioButton();
+            this.btnSocketServer = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.grpBroadcast.SuspendLayout();
@@ -51,9 +52,10 @@
             // 
             // btnStartServer
             // 
-            this.btnStartServer.Location = new System.Drawing.Point(44, 45);
+            this.btnStartServer.Location = new System.Drawing.Point(59, 55);
+            this.btnStartServer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStartServer.Name = "btnStartServer";
-            this.btnStartServer.Size = new System.Drawing.Size(108, 23);
+            this.btnStartServer.Size = new System.Drawing.Size(144, 28);
             this.btnStartServer.TabIndex = 0;
             this.btnStartServer.Text = "Start";
             this.btnStartServer.UseVisualStyleBackColor = true;
@@ -62,26 +64,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 22);
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.Size = new System.Drawing.Size(26, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Url";
             // 
             // txtUrl
             // 
-            this.txtUrl.Location = new System.Drawing.Point(44, 19);
+            this.txtUrl.Location = new System.Drawing.Point(59, 23);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUrl.Name = "txtUrl";
-            this.txtUrl.Size = new System.Drawing.Size(242, 20);
+            this.txtUrl.Size = new System.Drawing.Size(321, 22);
             this.txtUrl.TabIndex = 2;
             this.txtUrl.Text = "http://localhost:8080";
             // 
             // btnStop
             // 
             this.btnStop.Enabled = false;
-            this.btnStop.Location = new System.Drawing.Point(178, 45);
+            this.btnStop.Location = new System.Drawing.Point(237, 55);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(108, 23);
+            this.btnStop.Size = new System.Drawing.Size(144, 28);
             this.btnStop.TabIndex = 3;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
@@ -92,33 +97,39 @@
             this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtLog.Location = new System.Drawing.Point(8, 29);
+            this.txtLog.Location = new System.Drawing.Point(11, 36);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
             this.txtLog.ReadOnly = true;
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtLog.Size = new System.Drawing.Size(418, 205);
+            this.txtLog.Size = new System.Drawing.Size(556, 251);
             this.txtLog.TabIndex = 4;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.txtLog);
-            this.groupBox1.Location = new System.Drawing.Point(10, 236);
+            this.groupBox1.Location = new System.Drawing.Point(13, 290);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(444, 257);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(592, 316);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Log";
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.btnSocketServer);
             this.groupBox2.Controls.Add(this.txtUrl);
             this.groupBox2.Controls.Add(this.btnStartServer);
             this.groupBox2.Controls.Add(this.btnStop);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(10, 12);
+            this.groupBox2.Location = new System.Drawing.Point(13, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(444, 82);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(592, 101);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Server";
@@ -134,97 +145,118 @@
             this.grpBroadcast.Controls.Add(this.rdToGroup);
             this.grpBroadcast.Controls.Add(this.rdToAll);
             this.grpBroadcast.Enabled = false;
-            this.grpBroadcast.Location = new System.Drawing.Point(10, 100);
+            this.grpBroadcast.Location = new System.Drawing.Point(13, 123);
+            this.grpBroadcast.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grpBroadcast.Name = "grpBroadcast";
-            this.grpBroadcast.Size = new System.Drawing.Size(444, 130);
+            this.grpBroadcast.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpBroadcast.Size = new System.Drawing.Size(592, 160);
             this.grpBroadcast.TabIndex = 7;
             this.grpBroadcast.TabStop = false;
             this.grpBroadcast.Text = "Broadcast Message";
             // 
-            // rdToAll
-            // 
-            this.rdToAll.AutoSize = true;
-            this.rdToAll.Checked = true;
-            this.rdToAll.Location = new System.Drawing.Point(12, 22);
-            this.rdToAll.Name = "rdToAll";
-            this.rdToAll.Size = new System.Drawing.Size(52, 17);
-            this.rdToAll.TabIndex = 0;
-            this.rdToAll.TabStop = true;
-            this.rdToAll.Text = "To All";
-            this.rdToAll.UseVisualStyleBackColor = true;
-            // 
-            // rdToGroup
-            // 
-            this.rdToGroup.AutoSize = true;
-            this.rdToGroup.Location = new System.Drawing.Point(12, 50);
-            this.rdToGroup.Name = "rdToGroup";
-            this.rdToGroup.Size = new System.Drawing.Size(70, 17);
-            this.rdToGroup.TabIndex = 1;
-            this.rdToGroup.Text = "To Group";
-            this.rdToGroup.UseVisualStyleBackColor = true;
-            // 
-            // rdToClient
-            // 
-            this.rdToClient.AutoSize = true;
-            this.rdToClient.Location = new System.Drawing.Point(12, 76);
-            this.rdToClient.Name = "rdToClient";
-            this.rdToClient.Size = new System.Drawing.Size(67, 17);
-            this.rdToClient.TabIndex = 2;
-            this.rdToClient.Text = "To Client";
-            this.rdToClient.UseVisualStyleBackColor = true;
-            // 
-            // cmbGroups
-            // 
-            this.cmbGroups.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbGroups.FormattingEnabled = true;
-            this.cmbGroups.Location = new System.Drawing.Point(98, 44);
-            this.cmbGroups.Name = "cmbGroups";
-            this.cmbGroups.Size = new System.Drawing.Size(188, 21);
-            this.cmbGroups.TabIndex = 3;
-            // 
-            // cmbClients
-            // 
-            this.cmbClients.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbClients.FormattingEnabled = true;
-            this.cmbClients.Location = new System.Drawing.Point(98, 71);
-            this.cmbClients.Name = "cmbClients";
-            this.cmbClients.Size = new System.Drawing.Size(188, 21);
-            this.cmbClients.TabIndex = 4;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 103);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Message";
-            // 
-            // txtMessage
-            // 
-            this.txtMessage.Location = new System.Drawing.Point(98, 98);
-            this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(188, 20);
-            this.txtMessage.TabIndex = 7;
-            // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(301, 42);
+            this.btnSend.Location = new System.Drawing.Point(401, 52);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(131, 76);
+            this.btnSend.Size = new System.Drawing.Size(175, 94);
             this.btnSend.TabIndex = 9;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 127);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Message";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(131, 121);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(249, 22);
+            this.txtMessage.TabIndex = 7;
+            // 
+            // cmbClients
+            // 
+            this.cmbClients.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbClients.FormattingEnabled = true;
+            this.cmbClients.Location = new System.Drawing.Point(131, 87);
+            this.cmbClients.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbClients.Name = "cmbClients";
+            this.cmbClients.Size = new System.Drawing.Size(249, 24);
+            this.cmbClients.TabIndex = 4;
+            // 
+            // cmbGroups
+            // 
+            this.cmbGroups.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGroups.FormattingEnabled = true;
+            this.cmbGroups.Location = new System.Drawing.Point(131, 54);
+            this.cmbGroups.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbGroups.Name = "cmbGroups";
+            this.cmbGroups.Size = new System.Drawing.Size(249, 24);
+            this.cmbGroups.TabIndex = 3;
+            // 
+            // rdToClient
+            // 
+            this.rdToClient.AutoSize = true;
+            this.rdToClient.Location = new System.Drawing.Point(16, 94);
+            this.rdToClient.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdToClient.Name = "rdToClient";
+            this.rdToClient.Size = new System.Drawing.Size(85, 21);
+            this.rdToClient.TabIndex = 2;
+            this.rdToClient.Text = "To Client";
+            this.rdToClient.UseVisualStyleBackColor = true;
+            // 
+            // rdToGroup
+            // 
+            this.rdToGroup.AutoSize = true;
+            this.rdToGroup.Location = new System.Drawing.Point(16, 62);
+            this.rdToGroup.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdToGroup.Name = "rdToGroup";
+            this.rdToGroup.Size = new System.Drawing.Size(90, 21);
+            this.rdToGroup.TabIndex = 1;
+            this.rdToGroup.Text = "To Group";
+            this.rdToGroup.UseVisualStyleBackColor = true;
+            // 
+            // rdToAll
+            // 
+            this.rdToAll.AutoSize = true;
+            this.rdToAll.Checked = true;
+            this.rdToAll.Location = new System.Drawing.Point(16, 27);
+            this.rdToAll.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdToAll.Name = "rdToAll";
+            this.rdToAll.Size = new System.Drawing.Size(65, 21);
+            this.rdToAll.TabIndex = 0;
+            this.rdToAll.TabStop = true;
+            this.rdToAll.Text = "To All";
+            this.rdToAll.UseVisualStyleBackColor = true;
+            // 
+            // btnSocketServer
+            // 
+            this.btnSocketServer.Location = new System.Drawing.Point(425, 55);
+            this.btnSocketServer.Name = "btnSocketServer";
+            this.btnSocketServer.Size = new System.Drawing.Size(129, 28);
+            this.btnSocketServer.TabIndex = 4;
+            this.btnSocketServer.Text = "Connect Socket";
+            this.btnSocketServer.UseVisualStyleBackColor = true;
+            this.btnSocketServer.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FrmServer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(467, 508);
+            this.ClientSize = new System.Drawing.Size(623, 625);
             this.Controls.Add(this.grpBroadcast);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmServer";
             this.Text = "Server";
             this.groupBox1.ResumeLayout(false);
@@ -255,6 +287,7 @@
         private System.Windows.Forms.RadioButton rdToClient;
         private System.Windows.Forms.RadioButton rdToGroup;
         private System.Windows.Forms.RadioButton rdToAll;
+        private System.Windows.Forms.Button btnSocketServer;
     }
 }
 
--- WinFormsServer/FrmServer.cs
+++ WinFormsServer/FrmServer.cs
@@ -8,6 +8,10 @@ using Microsoft.Owin.Cors;
 using Microsoft.Owin.Hosting;
 using Microsoft.AspNet.SignalR;
 using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace WinFormsServer
 {
@@ -30,6 +34,12 @@ namespace WinFormsServer
             SimpleHub.ClientJoinedToGroup += SimpleHub_ClientJoinedToGroup;
             SimpleHub.ClientLeftGroup += SimpleHub_ClientLeftGroup;
             SimpleHub.MessageReceived += SimpleHub_MessageReceived;
+            StartSignalRServer();
+            ThreadStart threadStart = new ThreadStart(StartSocketServer);
+            Thread socketThread = new Thread(threadStart);
+            socketThread.Start();
+           
+            btnSocketServer.Enabled = false;
         }
 
         private void bindListsToControls()
@@ -115,19 +125,24 @@ namespace WinFormsServer
                 //Start SignalR server with the give URL address
                 //Final server address will be "URL/signalr"
                 //Startup.Configuration is called automatically
-                _signalR = WebApp.Start<Startup>(txtUrl.Text);
-
-                btnStartServer.Enabled = false;
-                txtUrl.Enabled = false;
-                btnStop.Enabled = true;
-                grpBroadcast.Enabled = true;
-
-                writeToLog($"Server started at:{txtUrl.Text}");
+                StartSignalRServer();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void StartSignalRServer()
+        {
+            _signalR = WebApp.Start<Startup>(txtUrl.Text);
+
+            btnStartServer.Enabled = false;
+            txtUrl.Enabled = false;
+            btnStop.Enabled = true;
+            grpBroadcast.Enabled = true;
+
+            writeToLog($"SignalR Server started at:{txtUrl.Text}");
         }
 
         private void btnStop_Click(object sender, EventArgs e)
@@ -175,6 +190,95 @@ namespace WinFormsServer
                 this.BeginInvoke(new Action(() => txtLog.AppendText(log + Environment.NewLine)));
             else
                 txtLog.AppendText(log + Environment.NewLine);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartSocketServer();
+        }
+
+        private void StartSocketServer()
+        {
+            // Establish the local endpoint  
+            // for the socket. Dns.GetHostName 
+            // returns the name of the host  
+            // running the application. 
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 60000);
+
+            // Creation TCP/IP Socket using  
+            // Socket Class Costructor 
+            Socket listener = new Socket(ipAddr.AddressFamily,
+                         SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                // Using Bind() method we associate a 
+                // network address to the Server Socket 
+                // All client that will connect to this  
+                // Server Socket must know this network 
+                // Address 
+                listener.Bind(localEndPoint);
+
+                // Using Listen() method we create  
+                // the Client list that will want 
+                // to connect to Server 
+                listener.Listen(10);
+                writeToLog("Socket Server Started.");
+                while (true)
+                {
+
+                    Console.WriteLine("Waiting connection ... ");
+
+                    // Suspend while waiting for 
+                    // incoming connection Using  
+                    // Accept() method the server  
+                    // will accept connection of client 
+                    Socket clientSocket = listener.Accept();
+
+                    // Data buffer 
+                    byte[] bytes = new Byte[1024];
+                    string data = null;
+
+                    while (true)
+                    {
+
+                        int numByte = clientSocket.Receive(bytes);
+                        if (numByte == 0)
+                            break;
+
+                        data += Encoding.ASCII.GetString(bytes,
+                                                   0, numByte);
+
+                       
+                    }
+
+                   
+                 //   byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+                    var hubContext = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+                    writeToLog($"Data Received:{data}");
+                    hubContext.Clients.All.addMessage("SERVER", data);
+                    writeToLog($"Socket Server is listning on localhost");
+                    // Send a message to Client  
+                    // using Send() method 
+                   // clientSocket.Send(message);
+
+                    // Close client Socket using the 
+                    // Close() method. After closing, 
+                    // we can use the closed Socket  
+                    // for a new Client Connection 
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
